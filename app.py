a499d565faab5ee5a664536c41928221bfa1c79e
Ruth Mahalla <rutimilano@gmail.com>
2022-02-05 21:30:54 +0200

    fix files before upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from consts import app, db, DB_PATH
-from flask import Flask, session, render_template, request, flash, redirect, url_for, jsonify
+from flask import session, render_template, request, flash, redirect, url_for
 from os import path
 from model import Exercise, User
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
@@ -27,20 +27,22 @@ def create_exercises_table():
         db.session.commit()
 
 
+def create_login_manager(app):
+    login_manager = LoginManager()
+    login_manager.login_view = 'login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+
 create_database()
 create_exercises_table()
+create_login_manager(app)
 
 
-login_manager = LoginManager()
-login_manager.login_view = 'login'
-login_manager.init_app(app)
-
-@login_manager.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
-
-# TODO 1. התרגילים והפתרונות לא מסונכרנים 2. איך להוסיף את הנתונים לטבלה המקשרת 3. ליצור עוד כדף בית שמופיעים בו כל היוזרים והתוצאות שלהם?
+#TODO  ליצור עוד כדף בית שמופיעים בו כל היוזרים והתוצאות שלהם????
 
 
 def get_random_exercises_dict():
@@ -59,8 +61,7 @@ exercises_dict = get_random_exercises_dict()
 @app.route('/', methods=['GET', 'POST'])  
 @login_required
 def home():
-    # if "user" in session: 
-    #     active_user = session["user"]
+
     if request.method == 'POST':
         entered_answer = request.form.get('answer')
         if not entered_answer:
@@ -75,36 +76,28 @@ def home():
             user.exercises.append(solved_ex)
             db.session.add(user)
             db.session.commit()
-
             session["current_question"] = str(int(session["current_question"]) + 1)
             
     if "current_question" not in session:
         session["current_question"] = "1"
 
-    elif session["current_question"] not in exercises_dict:   #TODO not sure it works
-        # If the current question number is not available in the questions
-        # dictionary, it means that the user has completed the quiz. So show
-        # the success page.
+    elif session["current_question"] not in exercises_dict:   # not sure it works
         return render_template("success.html", user=current_user)
     
-   # solved = "1111" # Exercise.query.join(Exercise.users).filter(User.name == 'bob')
-
     solved_exercises = User.query.filter_by(id=session["user_id"]).first().exercises
     solved = [exercise.ex for exercise in solved_exercises]
     score = sum([exercise.score for exercise in solved_exercises])
-
 
     return render_template("home.html", question=exercises_dict[session["current_question"]]["ex"], user=current_user, solved=solved, score=score)
        
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
     if request.method == 'POST':
         nickname = request.form.get('nickname')
-
         password = request.form.get('password')
         user = User.query.filter_by(name=nickname).first()
-        # session["user"] = user  
         session["user_id"] = user.id
         if user:
             if check_password_hash(user.password, password):
@@ -120,6 +113,7 @@ def login():
 
 
 @app.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for('login'))
@@ -127,6 +121,7 @@ def logout():
 
 @app.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+
     if request.method == 'POST':
         nickname = request.form.get('nickname')
         password1 = request.form.get('password1')
@@ -143,11 +138,10 @@ def sign_up():
             flash('Password must be at least 4 characters', category='error')
         else:
             new_user = User(name=nickname, password=generate_password_hash(password1, method='sha256'))
-            
             db.session.add(new_user)
             db.session.commit()
 
-            login_user(new_user, remember=True)
+            login_user(new_user)
             flash('Account created!', category='success')
             return redirect(url_for('home'))
 
--- consts.py
+++ consts.py
@@ -1,18 +1,19 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SECRETKEY = os.getenv("KEY")
 
 DB_PATH = 'sqlite:///C:\\Users\\israe\\homework\\week15\\multiplication_app\\multiplication.db'
 
 
 def init_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '1234'
-    # from views import views
-    # from auth import auth
-    # app.register_blueprint(views, url_prefix='/')
-    # app.register_blueprint(auth, url_prefix='/')
-
+    app.config['SECRET_KEY'] = SECRETKEY
     return app
 
 
@@ -24,49 +25,3 @@ def init_db_connection(flask_app):
 
 app = init_app()
 db = init_db_connection(app)
-
-
-
-
-
-
-
-
-
-
-# @app.route('/')
-# def home():
-#     return render_template("home.html")
-
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-
-#     return render_template("login.html")
-
-
-# @app.route('/logout')
-# def logout():
-#     return "logout"
-
-
-# @app.route('/sign-up', methods=['GET', 'POST'])
-# def sign_up():
-#     if request.method == 'POST':
-#         nickname = request.form.get('Nickname')
-#         password1 = request.form.get('password1')
-#         password2 = request.form.get('password2')
-
-#         if len(nickname) < 2:
-#             flash('Nickname must be at least 2 characters', category='error')
-#         elif password1 != password2:
-#             flash('Passwords don\'t match', category='error')
-#         elif len(password1) < 4:
-#             flash('Password must be at least 4 characters', category='error')
-#         else:
-#             flash('Account created', category='success')
-#         # new_user = User()
-#     return render_template("sign_up.html")
-
-
-# # app.run(debug=True)
